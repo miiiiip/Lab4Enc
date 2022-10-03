04fe40b4c245c3315a1e887b8a1476a4d1e65b6e
Kahnurd <hetti031@morris.umn.edu>
2022-10-03 17:12:03 -0500

    Make it so char[] is used in all functions + comments
    I just added comments to some of the functions that might not make sense
    just from looking at it. I also made sure all of the functions are only
    throwing around char arrays, because swapping betwen int and char can
    get confusing.

--- BLM.java
+++ BLM.java
@@ -15,9 +15,12 @@ public class BLM {
         char[] text = input.toCharArray();
         System.out.println("This is what the input looks like as a char Array: " + Arrays.toString(text));
         
-
-        int[] key = { 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0,
-                0, 1 };
+        // { 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1 };
+        // Make a string of 1's and 0's first and then convert to a char array cause this is easier than typing out 
+        // all the commas for the key. Also there is a weird interaction when typing integers into a char array 
+        // when trying to reconvert those integers into numbers again.
+        String keyTemp = "11000010110101101101001000110001001";
+        char[] key = keyTemp.toCharArray();
 
         System.out.println("Encrypted Text: ");
         System.out.println(Arrays.toString(runner.encrypt(text, key)));
@@ -26,11 +29,11 @@ public class BLM {
     }
 
 
-    public int[] shiftRightByThree(int[] input) {
+    public char[] shiftRightByThree(char[] input) {
         for (int J = 0; J < 3; J++) {
-            int store = input[0];
+            char store = input[0];
             for (int i = 0; i < input.length; i++) {
-                int temp = input[(i + 1) % input.length];
+                char temp = input[(i + 1) % input.length];
                 input[(i + 1) % input.length] = store;
                 store = temp;
             }
@@ -38,11 +41,11 @@ public class BLM {
         return input;
     }
 
-    public int[] shiftLeftByThree(int[] input) {
+    public char[] shiftLeftByThree(char[] input) {
         for (int J = 0; J < 3; J++) {
-            int store = input[input.length - 1];
+            char store = input[input.length - 1];
             for (int i = input.length - 1; i >= 0; i--) {
-                int temp = input[(i - 1 + input.length) % input.length];
+                char temp = input[(i - 1 + input.length) % input.length];
                 input[(i - 1 + input.length) % input.length] = store;
                 store = temp;
             }
@@ -50,38 +53,39 @@ public class BLM {
         return input;
     }
 
-    public int[] addToKey(int[] input, int[] key) {
-        int[] resultant = new int[key.length];
+    // This just adds the two char arrays together using weird notation found online to convert char into an integer
+    // and then convert it back to char for its actual value and not ASCII value (hence the + '0' at the end)
+    public char[] addToKey(char[] input, char[] key) {
+        char[] resultant = new char[key.length];
         for (int i = 0; i < resultant.length; i++) {
-            resultant[i] = (input[i] + key[i]) % 2;
+            // This turns into something like: (char)(((1 + 0)mod 2) + '0')
+            resultant[i] =  (char)(((Character.getNumericValue(input[i]) + Character.getNumericValue(key[i])) % 2) + '0');
         }
         return resultant;
     }
 
-    public String charToBinary(char[] input) {
-        String res = "";
+    // This takes an array of characters and converts each character into its binary representation
+    // The binary representation of each char is put into a string until all the chars are converted
+    // then the string gets converted into a char array of all the 1's and 0's obtained.
+    public char[] charToBinary(char[] input) {
+        String temp = "";
         for (int i = 0; i < input.length; i++) {
-            res = res + Integer.toBinaryString((int) input[i]);
+            temp = temp + Integer.toBinaryString((int) input[i]);
         }
+        char[] res = temp.toCharArray();
         return res;
     }
 
-    public int[] encrypt(char[] input, int[] key) {
-        String res = charToBinary(input);
-
-        char[] charArr = res.toCharArray();
-        int[] plain = new int[charArr.length];
-        for (int i = 0; i < charArr.length; i++) {
-            plain[i] = charArr[i];
-        }
-        plain = shiftRightByThree(plain);
-        int[] enc = new int[plain.length];
-        enc = addToKey(plain, key);
+    public char[] encrypt(char[] input, char[] key) {   
+        char[] charArr = charToBinary(input); // Convert to binary (Result is a char array of 1's and 0's)
+        char[] plain = shiftRightByThree(charArr); // Shift binary representation to the right (circular)
+        char[] enc = new char[plain.length]; 
+        enc = addToKey(plain, key); // Add the shifted binary represention of the text with the key (The result is still a char array of 1's and 0's)
         return (enc);
     }
 
-    public String decrypt(int[] encryptedText, int[] key){
-        int[] deKeyd = new int[key.length];
+    public String decrypt(char[] encryptedText, char[] key){
+        char[] deKeyd = new char[key.length];
         deKeyd = addToKey(encryptedText, key);
         deKeyd = shiftLeftByThree(deKeyd);
         String strRep = "";
